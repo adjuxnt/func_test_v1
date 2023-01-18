e776fae201b939a765f6d5f3dbd55607de2cd34b
adjuxnt <116542729+adjuxnt@users.noreply.github.com>
2023-01-18 10:28:08 +0100

    Create funksjonstesting2.js
    init

---
+++ www/js/funksjonstesting2.js
@@ -0,0 +1,14 @@
+// hvordan returnere data
+
+// definere funksjonen 
+function addOneTo(someNum) {
+	return someNum + 1;
+}
+
+// kjøre funksjonen...
+console.log( addOneTo(41) );
+
+// men hva skjer når man bruker noe annet enn et tall som argument?
+
+// tester tekst-streng
+console.log( addOneTo("hallo på do") );
