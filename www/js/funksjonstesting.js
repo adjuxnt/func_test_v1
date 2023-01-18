16ae120b5041ed4985e0ed13f8096dd5bf9c56b1
adjuxnt <116542729+adjuxnt@users.noreply.github.com>
2023-01-18 10:04:41 +0100

    Create funksjonstesting.js
    init

---
+++ www/js/funksjonstesting.js
@@ -0,0 +1,11 @@
+// definering av funk
+function skrivUtMin(setning) {
+	console.log(setning);
+};
+
+// oppkalling av funk
+skrivUtMin("Hallå från funksjone hehe");
+
+const finSetning = "Dette er en vakker setning";
+
+skrivUtMin(finSetning);
